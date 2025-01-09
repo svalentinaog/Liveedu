7f85b1d4d40d77b3b1bb1013f8d8f8f41ce6dd7c
svalentinaog <svalentinaog10@gmail.com>
2025-01-08 23:38:15 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { create, router as _router, defaults } from "json-server";
 const server = create();
-const router = _router("./src/modelsapi/db.json");
+const router = _router("./src/models/api/db.json");
 const middlewares = defaults();
 const port = process.env.PORT || 3000;
 
